6e2f04a4d98564a3ede7e83e96ca2f1a17c31f03
Serghei Iakovlev <serghei@phalconphp.com>
2017-03-18 20:56:25 +0200

    Improved install script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 INI_DIR=$(php -i | grep "Scan this dir for additional" | awk -F'=>' '{print $2}')
+PHPIZE_BIN=`which phpize`
+PHPCONFIG_BIN=`which php-config`
 
 sudo ulimit -c unlimited || true
+
+echo '/tmp/core_%e.%p' | sudo tee /proc/sys/kernel/core_pattern &> /dev/null
 echo "memory_limit=-1" | sudo tee ${INI_DIR}/000-initial.ini
 
 cd ext
@@ -12,11 +16,11 @@ export CFLAGS="-O0 -g3 -Wall -std=gnu90 -fvisibility=hidden -DZEPHIR_RELEASE=0"
 
 if [ -f Makefile ]; then
 	make --silent clean
-	phpize --silent --clean
+	${PHPIZE_BIN} --silent --clean
 fi
 
-phpize --silent
-./configure --silent --enable-group
+${PHPIZE_BIN} --silent
+./configure --silent --with-php-config=${PHPCONFIG_BIN} --enable-group
 
 make --silent
 sudo make --silent -j"$(getconf _NPROCESSORS_ONLN)" install
